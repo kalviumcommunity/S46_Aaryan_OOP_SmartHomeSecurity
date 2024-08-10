60b4f290a9595286dfe64723ef3cc22609ba6d37
aaryan <aaryanpanda04@gmail.com>
2024-08-10 10:32:38 +0530

    Implemented this pointer in classes for code clarity

--- src/DoorSensor.cpp
+++ src/DoorSensor.cpp
@@ -4,16 +4,13 @@
 DoorSensor::DoorSensor() : doorOpen(false) {}
 
 // methods with null return (void)
-void DoorSensor::toggleStatus() {
-    doorOpen = !doorOpen;
+void DoorSensor::toggleStatus()
+{
+    this->doorOpen = !this->doorOpen; // Using this pointer
 }
 
 // methods with a return value
-bool DoorSensor::checkStatus() const {
-    return doorOpen;
+bool DoorSensor::checkStatus() const
+{
+    return this->doorOpen; // Using this pointer
 }
-
-
-
-
-
--- src/MotionDetector.cpp
+++ src/MotionDetector.cpp
@@ -4,16 +4,19 @@
 MotionDetector::MotionDetector() : motionDetected(false) {}
 
 // methods with null return (void)
-void MotionDetector::detectMotion() {
-    motionDetected = true;
+void MotionDetector::detectMotion()
+{
+    this->motionDetected = true; // Using this pointer
 }
 
 // methods with a return value
-bool MotionDetector::getStatus() const {
-    return motionDetected;
+bool MotionDetector::getStatus() const
+{
+    return this->motionDetected; // Using this pointer
 }
 
 // methods with null return (void)
-void MotionDetector::reset() {
-    motionDetected = false;
+void MotionDetector::reset()
+{
+    this->motionDetected = false; // Using this pointer
 }
